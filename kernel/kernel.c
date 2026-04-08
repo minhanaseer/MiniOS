5243a8d0fc7b4b778db275e7572a55d2c79cc6af
minhanaseer <minhanaseerahmed@gmail.com>
2026-04-08 20:14:24 +0100

    commit

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -51,13 +51,6 @@ static void print_hex(uint32_t n) {
         putchar(hex[(n >> i) & 0xF], 0x0F);
 }
 
-static void print_num(uint32_t n) {
-    if (n == 0) { putchar('0', 0x0F); return; }
-    char buf[12]; int i = 0;
-    while (n > 0) { buf[i++] = '0'+(n%10); n/=10; }
-    for (int j=i-1;j>=0;j--) putchar(buf[j], 0x0F);
-}
-
 static unsigned char inb(unsigned short port) {
     unsigned char r;
     __asm__ volatile("inb %1,%0":"=a"(r):"Nd"(port));
@@ -76,56 +69,24 @@ static char sc_map[58] = {
     '*', 0, ' '
 };
 
-struct mb_info {
-    uint32_t flags;
-    uint32_t mem_lower;
-    uint32_t mem_upper;
-    uint32_t boot_device;
-    uint32_t cmdline;
-    uint32_t mods_count;
-    uint32_t mods_addr;
-    uint32_t syms[4];
-    uint32_t mmap_length;
-    uint32_t mmap_addr;
-    uint32_t drives_length;
-    uint32_t drives_addr;
-    uint32_t config_table;
-    uint32_t boot_loader_name;
-    uint32_t apm_table;
-    uint32_t vbe_control;
-    uint32_t vbe_mode_info;
-    uint16_t vbe_mode;
-    uint32_t fb_addr;
-    uint32_t fb_pitch;
-    uint32_t fb_width;
-    uint32_t fb_height;
-    uint8_t  fb_bpp;
-};
-
-void kernel_main(uint32_t magic, struct mb_info* mb) {
-    (void)magic;
+void kernel_main(uint32_t magic, uint32_t mb_addr) {
     clear();
+    print("=== MyKernel Debug ===", 0x0B); newline();
+    print("magic: ", 0x0F); print_hex(magic); newline();
+    print("mb_addr: ", 0x0F); print_hex(mb_addr); newline();
 
-    print("=== MyKernel FB Debug ===", 0x0B); newline();
-
-    print("flags : ", 0x0F); print_hex(mb->flags);   newline();
-    print("fb_addr : ", 0x0F); print_hex(mb->fb_addr); newline();
-    print("fb_w  : ", 0x0F); print_num(mb->fb_width);  newline();
-    print("fb_h  : ", 0x0F); print_num(mb->fb_height); newline();
-    print("fb_bpp: ", 0x0F); print_num(mb->fb_bpp);    newline();
-    print("fb_pitch: ", 0x0F); print_num(mb->fb_pitch); newline();
+    if (magic != 0x2BADB002) {
+        print("ERROR: Bad multiboot magic!", 0x04); newline();
+    } else {
+        print("Multiboot OK!", 0x0A); newline();
+    }
 
     newline();
-    print("If fb_addr is 0 = GRUB not passing framebuffer", 0x0E);
-    newline();
-    print("If fb_w is 0 = graphics mode not set", 0x0E);
-    newline();
-
     pmm_init(0);
     fs_init();
     loader_init();
     shell_init();
-    newline();
+    print("MyKernel v0.1 - Ready!", 0x0A); newline();
     print("> ", 0x0F);
 
     unsigned char last_sc = 0;
